0109c492c9cc0348d872cd20e30a7409aa7d2aa8
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2019-07-22 16:24:39 -0400

    Map Changes
    Finished Level 1 Map

---
+++ World Map1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="World Map1" tilewidth="16" tileheight="16" tilecount="140" columns="14">
+ <image source="World Map1.png" trans="757575" width="237" height="165"/>
+</tileset>
