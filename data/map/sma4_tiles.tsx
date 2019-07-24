1864a11414c21f307b7ecfcee7bb1df36bcaf69e
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2019-07-24 09:15:46 -0400

    map changes
    fixed map tilesets

--- data/map/sma4_tiles.tsx
+++ data/map/sma4_tiles.tsx
@@ -1,4 +1,47 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="sma4_tiles" tilewidth="16" tileheight="16" tilecount="1708" columns="61">
+<tileset version="1.2" tiledversion="1.2.4" name="sma4_tiles" tilewidth="18" tileheight="18" tilecount="1350" columns="54">
  <image source="../img/map/sma4_tiles.png" trans="757575" width="982" height="452"/>
+ <tile id="280">
+  <animation>
+   <frame tileid="280" duration="150"/>
+   <frame tileid="281" duration="150"/>
+   <frame tileid="282" duration="150"/>
+   <frame tileid="283" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="334">
+  <animation>
+   <frame tileid="334" duration="150"/>
+   <frame tileid="335" duration="150"/>
+   <frame tileid="336" duration="150"/>
+   <frame tileid="337" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="442">
+  <animation>
+   <frame tileid="442" duration="150"/>
+   <frame tileid="443" duration="150"/>
+   <frame tileid="444" duration="150"/>
+   <frame tileid="445" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="658">
+  <animation>
+   <frame tileid="658" duration="150"/>
+   <frame tileid="659" duration="150"/>
+   <frame tileid="660" duration="150"/>
+   <frame tileid="661" duration="150"/>
+   <frame tileid="712" duration="150"/>
+   <frame tileid="713" duration="150"/>
+   <frame tileid="714" duration="150"/>
+   <frame tileid="715" duration="150"/>
+  </animation>
+ </tile>
+ <tile id="770">
+  <animation>
+   <frame tileid="770" duration="150"/>
+   <frame tileid="824" duration="150"/>
+   <frame tileid="878" duration="150"/>
+  </animation>
+ </tile>
 </tileset>
