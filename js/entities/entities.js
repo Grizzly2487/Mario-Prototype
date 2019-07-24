8fe656814ae9d911df2936fd551aee26006be3f3
Odin519 <52928858+Odin519@users.noreply.github.com>
2019-07-24 12:13:14 -0400

    Merged entities.js
    Merged Norman's code and my own, also commented out the addAnimation codes, due to there being no actual character

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -23,12 +23,12 @@ game.PlayerEntity = me.Entity.extend({
         //TODO: Add Animation to player
         
         //define a basic animation (using all frames)
-        this.renderable.addAnimation("walk", [0,1,2,3,4,5,6,7])
+        //this.renderable.addAnimation("walk", [0,1,2,3,4,5,6,7])
         
-        this.renderable.addAnimation("stand", [0])
+        //this.renderable.addAnimation("stand", [0])
         
         //set standing animation as default  
-        this.renderable.setCurrentAnimation("stand")
+        //this.renderable.setCurrentAnimation("stand")
     },
     
     /**
@@ -42,28 +42,28 @@ game.PlayerEntity = me.Entity.extend({
                 this.renderable.flipX(true);
                 this.body.force.x = -this.body.maxVel.x;
                 //change to walking animation
-                if(!this.renderable.isCurrentAnimation("walk"))
-                {
-                this.renderable.setCurrentAnimation("walk");
-                }
+                //if(!this.renderable.isCurrentAnimation("walk"))
+                //{
+                //this.renderable.setCurrentAnimation("walk");
+                //}
             }
             else if(me.input.isKeyPressed('right'))
             {
                 this.renderable.flipX(false);
                 this.body.force.x = this.body.maxVel.x;
-                if(!this.renderable.isCurrentAnimation("walk"))
-                {
-                this.renderable.setCurrentAnimation("walk");
-                }
+                //if(!this.renderable.isCurrentAnimation("walk"))
+                //{
+                //this.renderable.setCurrentAnimation("walk");
+                //}
                 
             }
             else
             {
                 this.body.force.x = 0;
-                if(!this.renderable.isCurrentAnimation("stand"))
-                {
-                this.renderable.setCurrentAnimation("stand");
-                }
+                //if(!this.renderable.isCurrentAnimation("stand"))
+                //{
+                //this.renderable.setCurrentAnimation("stand");
+                //}
             }
        if(me.input.isKeyPressed('jump'))
         {
@@ -102,10 +102,10 @@ game.PlayerEntity = me.Entity.extend({
 
 game.CoinEntity = me.CollectableEntity.extend({
 
-    // extending the init function is not mandatory
+     // extending the init function is not mandatory
     //unles you need to add some extra initialization
      
-    init:function (x, y, settings) {
+     init:function (x, y, settings) {
         // call the  parent constructor
         this._super(me.CollectableEntity, 'init', [x, y, settings]);
     },
@@ -123,6 +123,60 @@ game.CoinEntity = me.CollectableEntity.extend({
         return false
     }
 });
+
+game.KoopaEntity = me.Entity.extend({
+    /**
+     * constructor
+     */
+    
+    init:function (x, y, settings) {
+        // call the constructor
+        this._super(me.Entity, 'init', [x, y , settings]);
+        
+        this.body.setMaxVelocity(3,15);
+        this.body.setFriction(0.4, 0);
+        
+        //set display to follow our postion on both axis
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH, 0.4);
+        
+        //ensure the player is updated even when outside of the viewport
+        this.alwaysUpdate = true;
+        
+        //TODO: add an animation to player
+    },
+
+    /**
+     * update the entity
+     */
+    update : function (dt) {
+        //if(me.)
+        //{
+            //this.body.force.x = -this.body.maxVel.x;    
+        //}
+         //else
+        //{
+            this.body.force.x = this.body.maxVel.x;
+        //}
+        // apply physics to the body (this moves the entity)
+        this.body.update(dt);
+
+        // handle collisions against other shapes
+        me.collision.check(this);
+
+        // return true if we moved or if the renderable was updated
+        return (this._super(me.Entity, 'update', [dt]) || this.body.vel.x !== 0 || this.body.vel.y !== 0);
+    },
+
+   /**
+     * colision handler
+     * (called when colliding with other objects)
+     */
+    onCollision : function (response, other) {
+        // Make all other objects solid
+        return true;
+    }
+});
+
         
         
         
--- js/game.js
+++ js/game.js
@@ -26,13 +26,14 @@ var game = {
 
     // Run on game resources loaded.
     "loaded" : function () {
-        me.state.set(me.state.MENU, new game.TitleScreen());
+        //me.state.set(me.state.MENU, new game.TitleScreen());
         me.state.set(me.state.PLAY, new game.PlayScreen());
 
         // add our player entity in the entity pool
         me.pool.register("Mario", game.PlayerEntity);       
         me.pool.register("Goomba", game.PlayerEntity);
         me.pool.register("Koopa", game.PlayerEntity);
+        me.pool.register("Turtle", game.PlayerEntity);
         
         //enable the keyboard
         me.input.bindKey(me.input.KEY.LEFT, "left");
