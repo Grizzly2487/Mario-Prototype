5b2fbbc43dbefaf55c2d46f62fc6152b2ec40a6b
USER <user@desktop-vuud826.trios.com>
2019-07-30 10:44:12 -0400

    enemy entities movement
    -made changes on goomba jumping movement to  go back and forth on a staright line.
    -slowed the velocity on both goomba and koopa

--- js/entities/enemies.js
+++ js/entities/enemies.js
@@ -28,7 +28,7 @@ game.KoopaEntity = me.Entity.extend(
         //add a default collision shape
         this.body.addShape(new me.Rect(0,0,this.width,this.height));
         
-        this.body.setMaxVelocity(3,15);
+        this.body.setMaxVelocity(1,7);
         this.body.setFriction(0.7, 0);
         //enable physic collision (off by default for basic me.Renderable
         this.isKinematic = false;
@@ -195,7 +195,7 @@ game.GoombaEntity = me.Entity.extend({
         
         //adjust the setting information to match sprite size
         settings.framewidth = settings.width = 17;
-        settings.frameheight = settings.height = 27;
+        settings.frameheight = settings.height = 17;
         
         // call the parent constructor
         this._super(me.Entity, 'init', [x, y, settings]);
@@ -205,7 +205,7 @@ game.GoombaEntity = me.Entity.extend({
         //add a default collision shape
         this.body.addShape(new me.Rect(0,0,this.width,this.height));
         
-        this.body.setMaxVelocity(3,15);
+        this.body.setMaxVelocity(1,7);
         this.body.setFriction(0.7, 0);
         //enable physic collision (off by default for basic me.Renderable
         this.isKinematic = false;
@@ -283,7 +283,110 @@ game.GoombaEntity = me.Entity.extend({
         return true;
     }
 });       
+game.FlyingGoombaEntity = me.Entity.extend({
+    /**
+     * constructor
+     */
+    
+     init:function (x, y, settings) {
+        // call the constructor
+        
+        // save the area size as defined in tile
+        var width = settings.width
+        
+        //define this here instead instead of tiled   
+        settings.image ="Flying Goomba.png";
+        
+        //adjust the setting information to match sprite size
+        settings.framewidth = settings.width = 17;
+        settings.frameheight = settings.height = 50;
+        
+        // call the parent constructor
+        this._super(me.Entity, 'init', [x, y, settings]);
+        
+        // add a physics body
+        this.body = new me.Body(this);
+        //add a default collision shape
+        this.body.addShape(new me.Rect(0,0,this.width,this.height));
+        
+        this.body.setMaxVelocity(1,7);
+        this.body.setFriction(0.7, 0);
+        //enable physic collision (off by default for basic me.Renderable
+        this.isKinematic = false;
+        
+        // set start and end position on the initial area size
+        x=this.pos.x;
+        this.StartX = x;
+        this.pos.x = this.endX = x + width - this.width;
+        // to remeber which side we were walking
+        this.walkLeft = false;
+        //make it alive
+        this.alive = true;
+    },
+    
+    /**
+     * update the entity
+     */
+    update : function (dt) 
+    {
+        if(this.alive)
+        {
+            if(this.walkLeft && this.pos.x <= this.StartX)
+                {
+                    this.walkLeft = false;
+                    this.body.force.x = this.body.maxVel.x;
+                }
+                else if (!this.walkLeft && this.pos.x >= this.endX)    
+                {
+                    this.walkLeft = true
+                    this.body.force.x = -this.body.maxVel.x;
+                }
+            
+            
+            this.renderable.flipX(this.walkLeft);
+        }
+        else
+        {
+            this.body.force.x = 0 
+            
+        }
+            
+        
+        // apply physics to the body (this moves the entity)
+        this.body.update(dt);
 
+        // handle collisions against other shapes
+        me.collision.check(this);
+
+        // return true if we moved or if the renderable was updated
+        return (this._super(me.Entity, 'update', [dt]) || this.body.vel.x !== 0 || this.body.vel.y !== 0);
+    },
+
+   /**
+     * collision handler
+     * (called when colliding with other objects)
+     */
+    onCollision : function (response, other) 
+    {
+        
+        if (response.b.body.collisionType !== me.collision.types.WORLD_SHAPE)
+        {
+            
+            //res.y>0 this means touched by something on the bottom
+            //which means top position for this one
+            
+            if(this.alive && (response.overlapV.y > 0)&& 
+                response.a.body.falling)
+            {
+                //this.renderable.flicker(750)
+            }
+                return false;
+        }
+        
+        // Make all other objects solid
+        return true;
+    }
+});       
 game.PirhanaPlant = me.Entity.extend({
 
      init: function (x, y, settings)
