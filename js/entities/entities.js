36cb1401add982608cb2cd88c259fcbf2b11cd26
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2019-07-30 10:30:06 -0400

    Made Changes
    World Changes to enemy plants
    World Select Changes
    Added mario and level 1 enetities in world select map

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -280,6 +280,155 @@ game.ItemBlocksEntity = me.Entity.extend({
         this._super(me.Entity, 'init', [x, y , settings]);
     }
 });
+game.Level1 = me.Entity.extend({
+    /**
+     * constructor
+     */
+    
+    init:function (x, y, settings)
+    {
+        // call the constructor
+        this._super(me.Entity, 'init', [x, y , settings]);
+    }
+});
+
+game.MarioLevelSelectEntity = me.Entity.extend({
+
+    /**
+     * constructor
+     */
+    init:function (x, y, settings) {
+        // call the constructor
+        this._super(me.Entity, 'init', [x, y, settings]);
+        
+        this.body.setMaxVelocity(1.5,1.5);
+        this.body.setFriction(0.4, 0.4);
+        this.body.gravity = 0;
+        
+        //set the display to follow our position on both axis
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH, 0.4);
+        
+        //ensure that the player is updated even when outside of the viewport
+        this.alwaysUpdate = true;
+        
+        
+        //TODO: Add Animation to player
+        
+        //define a basic animation (using all frames)
+        this.renderable.addAnimation("walk", [0,1])
+        
+        this.renderable.addAnimation("stand", [0])
+        
+        //set standing animation as default  
+        this.renderable.setCurrentAnimation("stand")
+    },
+    
+    /**
+     * update the entity
+     */
+
+    update : function (dt) 
+    {
+        if(me.input.isKeyPressed('left'))
+            {
+                this.renderable.flipX(true);
+                this.body.force.x = -this.body.maxVel.x;
+                //change to walking animation
+                if(!this.renderable.isCurrentAnimation("walk"))
+                {
+                this.renderable.setCurrentAnimation("walk");
+                }
+            }
+            else if(me.input.isKeyPressed('right'))
+            {
+                this.renderable.flipX(false);
+                this.body.force.x = this.body.maxVel.x;
+                if(!this.renderable.isCurrentAnimation("walk"))
+                {
+                this.renderable.setCurrentAnimation("walk");
+                }
+                
+            }
+            else
+            {
+                this.body.force.x = 0;
+                if(!this.renderable.isCurrentAnimation("stand"))
+                {
+                this.renderable.setCurrentAnimation("stand");
+                }
+            }
+       if (me.input.isKeyPressed('jump')) 
+      {
+          this.body.force.y = this.body.maxVel.y;
+      } 
+        else 
+      {
+          this.body.force.y = 0;
+      }
+        // apply physics to the body (this moves the entity)
+        this.body.update(dt);
+
+        // handle collisions against other shapes
+        me.collision.check(this);
+
+        // return true if we moved or if the renderable was updated
+        return (this._super(me.Entity, 'update', [dt]) || this.body.vel.x !== 0 || this.body.vel.y !== 0);
+    },
+
+   /**
+     * colision handler
+     * (called when colliding with other objects)
+     */
+    onCollision : function (response, other) {
+        
+         switch(response.b.body.collisionType)
+        {
+                
+            case me.collision.types.WORLD_SHAPE:
+                
+                //simulate platform object
+                if(other.type === "platform")
+                 {
+                     if(this.body.falling && !me.input.isKeyPressed('down') && response.overlapV.y > 0 && ~~this.body.vel.y >= ~~response.overlapV.y)
+                     {
+                         response.overlapV.x = 0;
+                         //respond to the platform it is solid
+                    // Make all other objects solid
+                    return true;
+                    }
+                     //Do not repond to the platform pass through
+                     return false;
+                 }
+                break;
+            case me. collision.types.ENEMY_OBJECT:
+                    if( (response.overlapV.y > 0) && !this.body.jumping)
+                        {
+                            // make sure were on top of the other object
+                            this.pos.y = other.pos.y - this.height - 2;
+                            
+                            //bounce (force jump)
+                            this.body.falling = false
+                            this.body.vel.y = -this.body.maxVel.y * me.timer.tick;
+                            
+                            //set the jumping flag
+                            this.body.jumping = true;                           
+                        }
+                        else
+                        {
+                            this.renderable.flicker(750);
+                        }
+                    
+            default:
+                    //Do not respond to other objects (e.g.Coins)
+                    return false;
+        }
+        
+        // Make all other objects solid
+        return true;
+    
+    }
+});
+
        
         
         
