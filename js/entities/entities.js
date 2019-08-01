23af2a02a1e7d62b6307063bac9c26c08e697872
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2019-08-01 09:50:04 -0400

    Changes - Marios Jump/Run
    Norman made the code i just tweaked the numbers to respond allitle better, thank you norman.

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -12,7 +12,7 @@ game.PlayerEntity = me.Entity.extend({
         // call the constructor
         this._super(me.Entity, 'init', [x, y, settings]);
         
-        this.body.setMaxVelocity(1.7,13);
+        this.body.setMaxVelocity(1.5,15);
         this.body.setFriction(0.4, 0);
         
         //set the display to follow our position on both axis
@@ -41,11 +41,11 @@ game.PlayerEntity = me.Entity.extend({
     {
         if(me.input.isKeyPressed('run'))
         {
-            this.body.setMaxVelocity(2.2,12.2);
+            this.body.setMaxVelocity(2.5,18);
             this.body.setFriction(0.4,0.3);
         }
         else{
-            this.body.setMaxVelocity(1.7,13);
+            this.body.setMaxVelocity(1.5,15);
             this.body.setFriction(0.4, 0.3);
         }
             
